a87282e4675257b26c5b93a3f55e2d1ab5f86000
Nikos Mouchtaris <mouchtaris@gmail.com>
2015-12-24 21:42:17 +0000

    Proper require of logger

--- lib/main.rb
+++ lib/main.rb
@@ -8,7 +8,7 @@ require 'tilt/sass'
 require 'uri'
 require 'yaml'
 
-require_relative 'logger'
+require_relative '../lib/logger'
 require_relative 'pacache'
 require_relative 'server'
 require_relative 'consolidator'
