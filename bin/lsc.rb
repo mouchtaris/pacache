73d3437d882f940da301d627ed20f4a9a77d8d7d
Nikos Mouchtaris <mouchtaris@gmail.com>
2016-01-02 19:33:14 +0000

    Proper new-index sorting

--- bin/lsc.rb
+++ bin/lsc.rb
@@ -2,7 +2,7 @@ require_relative 'main'
 
 new_main.each_new_with_source.
   map { |y, s|
-    sid = /new\/(\d+)$/.match(s)[1]
+    sid = /new\/(\d+)$/.match(s)[1].to_i
     key = y.keys.first
     rx = /.*\/([.\w\d-]+)$/
     md = rx.match(key)
@@ -14,4 +14,4 @@ new_main.each_new_with_source.
     [ k, ns[0], ns[1] ]
   }.
   sort_by { |k,| k }.
-  each { |k, n, v| printf '%02d: %10s %s%s', k, v, n, "\n" }
+  each { |k, n, v| printf '%03d: %10s %s%s', k, v, n, "\n" }
